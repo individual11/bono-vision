e11834a37e6b7f4ccd2579971ea30d532c9b41ff
individual11 <david@individual11.com>
2014-02-04 18:23:07 -0500

    more explanation of the parts

--- bookmarklet/bono-vision.js
+++ bookmarklet/bono-vision.js
@@ -1,10 +1,13 @@
 //create the div we need
 var bono = document.createElement("div");
+
 //color purple we are using
 bono.style.background = '#820BBB';
+
 //how transparent we want it
-bono.style.opacity = 0.4;
-bono.style.filter = "alpha(opacity=40)";
+bono.style.opacity = 0.0;
+bono.style.filter = "alpha(opacity=00)";
+
 //dimensions and positioning
 bono.style.height = '100%';
 bono.style.width = '100%';
@@ -16,6 +19,7 @@ bono.style.zIndex = 4242;
 //makes it so you can still click through
 bono.style.pointerEvents = 'none';
 
+//helps insure it's full screen
 document.body.style.height= '100%';
 document.body.style.width = '100%';
 document.body.parentElement.style.position = 'relative';
@@ -23,24 +27,20 @@ document.body.parentElement.style.position = 'relative';
 //prepend it to the body
 document.body.insertBefore(bono, document.body.firstChild);
 
-//fadein code
+//fadein code (courtesy of http://youmightnotneedjquery.com/)
 function fadeIn(el) {
   el.style.opacity = 0;
 
   var last = +new Date;
   var tick = function() {
     el.style.opacity = Number(el.style.opacity) + (new Date - last) / 400;
-
     last = +new Date;
-
-
-    if (el.style.opacity < 1){
+    if (el.style.opacity < 0.4){
       (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
     }
   }
-  	
-
   tick();
 }
 
-//fadeIn(bono);
+//now fade it in
+fadeIn(bono);
