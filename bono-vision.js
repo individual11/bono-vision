425587da817fb8e07560c970960db1c45b1e21da
individual11 <david@individual11.com>
2014-02-16 20:49:51 -0500

    made it so the height is confirmed against the html and body elements, and added a scroll event to make sure it continues to adjust to the right height for infinite scroll sites

--- bono-vision.js
+++ bono-vision.js
@@ -1,4 +1,10 @@
 if(!document.querySelectorAll('#bono_vision').length){
+  var bono_body = document.body,
+      bono_html = document.documentElement;
+
+  //keeps track if we need to change the height later
+  var bono_height = -1;
+
   //create the div we need
   var bono = document.createElement("div");
   bono.setAttribute('id', 'bono_vision');
@@ -12,23 +18,29 @@ if(!document.querySelectorAll('#bono_vision').length){
   bono.style.filter = "alpha(opacity=00)";
 
   //dimensions and positioning
-  bono.style.height = '100%';
   bono.style.width = '100%';
   bono.style.position = 'absolute';
   bono.style.top = 0;
   bono.style.left = 0;
   bono.style.zIndex = 4242;
 
+  //set the height
+  extendOverlay(bono);
+
   //makes it so you can still click through
   bono.style.pointerEvents = 'none';
 
   //helps insure it's full screen
-  document.body.style.height= '100%';
-  document.body.style.width = '100%';
-  document.body.parentElement.style.position = 'relative';
+  bono_body.style.height= '100%';
+  bono_body.style.width = '100%';
+  bono_html.style.position = 'relative';
 
   //prepend it to the body
-  document.body.insertBefore(bono, document.body.firstChild);
+  bono_body.insertBefore(bono, document.body.firstChild);
+
+  ////////////
+  //FUNCTIONS
+  ///////////
 
   //fadein code (courtesy of http://youmightnotneedjquery.com/)
   function fadeIn(el, endTransparency) {
@@ -45,6 +57,21 @@ if(!document.querySelectorAll('#bono_vision').length){
     tick();
   }
 
+  //make overlay the right size
+  function extendOverlay(el){
+    var maxHeight = Math.max( bono_body.scrollHeight, bono_body.offsetHeight, bono_html.clientHeight, bono_html.scrollHeight, bono_html.offsetHeight );
+    if(bono_height !== maxHeight){
+      var height_str = (maxHeight)? String(maxHeight + 'px'):'100%';
+      el.style.height = height_str;
+      bono_height = maxHeight;
+    }
+  }
+
   //now fade it in
   fadeIn(bono, maxTransparency);
+
+  //when the user scrolls, adjust the height if needed (fixes infinite scroll sites)
+  window.addEventListener("scroll", function(evt) {
+    extendOverlay(bono);
+  });
 }
